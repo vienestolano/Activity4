56538eb37cfcdc7c7b888e9013242d8b9f041233
JakeLaylo <laylojake01@gmail.com>
2023-11-09 16:25:00 +0800

    feat/create table owners

--- veterinary.sql
+++ veterinary.sql
@@ -1,1 +1,10 @@
 CREATE DATABASE veterinary;
+
+CREATE TABLE owners( 
+    ownerid INT PRIMARY KEY, 
+    ofirstname VARCHAR(50) NOT NULL,
+    phone VARCHAR(15) NOT NULL, 
+    phone VARCHAR(15) NOT NULL,
+    olastname VARCHAR(50) NOT NULL, 
+    address VARCHAR(15) NOT NULL, 
+    email VARCHAR(100) NOT NULL );
