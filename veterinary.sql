8085cb209a30be0bc069622e85735e2be1d13c67
JakeLaylo <laylojake01@gmail.com>
2023-11-09 17:55:54 +0800

    insert into doctors

--- veterinary.sql
+++ veterinary.sql
@@ -151,3 +151,11 @@ JOIN appointments ap
 GROUP BY a.animalid, a.name
 ORDER BY appointment_count DESC
 LIMIT 1;
+
+INSERT INTO doctors(doctorid,dfirstname,dlastname,speciality,phone,email)
+VALUES(1,'Dr.Maria','Santos','General Veterinarian, 987-654-3210,'maria@example.com'),
+(2,'Dr. Antonio','Gonzales','Feline Specialist', 555-123-4567,'antonio@example.com'),
+(3,'Dr. Felipe','Luna','Orthopedic Specialist', 111-222-3333,'felipe@example.com'),
+(4,'Dr. Sofia','Reyes','Dermatology Specialist', 999-888-7777,'sofia@example.com'),
+(5,'Dr. Luis','Torres','Surgery Specialist', 123-555-7777,'luis@example.com'),
+(6,'Dr. Carmen','Fernandez','Ophthalmology Specialist', 333-222-1111,'carmen@example.com');
