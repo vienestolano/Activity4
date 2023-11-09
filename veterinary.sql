69d23a1cd32a1c1b2f3215d8c4c70e3a98ff4a79
JakeLaylo <laylojake01@gmail.com>
2023-11-09 17:50:37 +0800

    select count appointments

--- veterinary.sql
+++ veterinary.sql
@@ -131,3 +131,15 @@ FROM animals;
 
 SELECT SUM(totalamount) AS total_sales
 FROM invoices;
+
+SELECT COUNT(*) AS total_appointments
+FROM appointments
+WHERE animalid IN (
+    SELECT animalid
+    FROM animals
+    WHERE ownerid = (
+        SELECT ownerid
+        FROM owners
+        WHERE ofirstname = 'Maria'
+    )
+);
